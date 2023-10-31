1bd903caaf775963b2a82dadb46cb0a06f675928
AhmetEminOzturk <eminahmetozturk@gmail.com>
2023-10-31 16:39:34 +0300

    serviceLayer

---
+++ NLayer.Core/DTOs/CustomResponseDto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.DTOs
+{
+    public class CustomResponseDto<T>
+    {
+        public T Data { get; set; }
+
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+
+        public List<String> Errors { get; set; }
+
+        public static CustomResponseDto<T> Success(int statusCode, T data)
+        {
+            return new() { Data = data, StatusCode = statusCode, Errors = null };
+        }
+        public static CustomResponseDto<T> Success(int statusCode)
+        {
+            return new() { StatusCode = statusCode };
+        }
+        public static CustomResponseDto<T> Fail(int statusCode, List<String> errors)
+        {
+            return new() { StatusCode = statusCode, Errors = errors };
+        }
+        public static CustomResponseDto<T> Fail(int statusCode, string errors)
+        {
+            return new() { StatusCode = statusCode, Errors = new List<string> { errors } };
+        }
+
+    }
+}
